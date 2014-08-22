92216e80066709e0638b7a2abc21391f24a12f18
Ben D'Herville <ben.dherville@encompasscorporation.com>
2014-08-22 12:38:28 +1000

    ENC-3653: Add some assertions

--- src/main/java/org/springframework/social/salesforce/api/impl/ApexRestTemplate.java
+++ src/main/java/org/springframework/social/salesforce/api/impl/ApexRestTemplate.java
@@ -3,9 +3,14 @@ package org.springframework.social.salesforce.api.impl;
 import org.springframework.social.salesforce.api.ApexRestOperations;
 import org.springframework.social.salesforce.api.Salesforce;
 import org.springframework.social.support.URIBuilder;
+import org.springframework.util.Assert;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
+
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+import static org.springframework.util.Assert.isTrue;
+import static org.springframework.util.Assert.notNull;
 
 public class ApexRestTemplate extends AbstractSalesForceOperations<Salesforce> implements ApexRestOperations {
 
@@ -21,6 +26,10 @@ public class ApexRestTemplate extends AbstractSalesForceOperations<Salesforce> i
 
     @Override
     public <T> T get(String resource, String identifier, Class<T> resultType) {
+        Assert.isTrue(isNotBlank(resource), "Resource must not be blank");
+        Assert.isTrue(isNotBlank(identifier), "Identifier must not be blank");
+        Assert.notNull(resultType, "Result type must not be null");
+
         URI uri = URIBuilder.fromUri(api.getInstanceUrl() + APEX_REST_PATH + resource + PATH_SEPARATOR + identifier).build();
         return restTemplate.getForObject(uri, resultType);
     }
