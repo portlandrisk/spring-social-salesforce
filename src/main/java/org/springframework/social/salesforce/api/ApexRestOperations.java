701d394bcd03370293405246f71acb1fbe602684
Ben D'Herville <ben.dherville@encompasscorporation.com>
2014-09-01 09:47:10 +1000

    ENC-3661: Add APEX post operation

--- src/main/java/org/springframework/social/salesforce/api/ApexRestOperations.java
+++ src/main/java/org/springframework/social/salesforce/api/ApexRestOperations.java
@@ -3,4 +3,6 @@ package org.springframework.social.salesforce.api;
 public interface ApexRestOperations {
 
     <T> T get(String resource, String identifier, Class<T> resultType);
+
+    <T> T post(String resource, Object requestData, Class<T> resultType);
 }
